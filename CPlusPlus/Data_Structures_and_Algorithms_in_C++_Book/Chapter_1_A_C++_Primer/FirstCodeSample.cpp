17f5edc1922aa164453db123f1acc34c67cf34ac
AdhamNour <adhamnourelwaffaa@gmail.com>
2020-06-07 20:35:33 +0200

    Finished First 1.1.2 Fundamental Types

---
+++ CPlusPlus/Data_Structures_and_Algorithms_in_C++_Book/Chapter_1_A_C++_Primer/FirstCodeSample.cpp
@@ -0,0 +1,12 @@
+#include<cstdlib>
+#include<iostream>
+int main(){
+/*This Program inputs two numbers x and y and outputs their sum*/
+	int x,y;
+	std::cout<<"Enter two Number To be Summed"<<std::endl;
+	std::cin>>x>>y;
+	int sum = x+y;
+	std::cout<<"The Sum is "<<sum<<std::endl;
+	return EXIT_SUCCESS;
+/*this code appears  in page no 3 in the book*/
+}
